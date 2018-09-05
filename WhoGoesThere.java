7aaa0ecde352b625195e97b6e17382c758fe8cfe
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-09-05 17:59:34 -0500

    WhoIsIt

---
+++ WhoGoesThere.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class WhoGoesThere {
+	public static void main (String[] args) {
+		String first, last;
+		int age;
+		
+		Scanner inputDevice = new Scanner (System.in);
+		
+		System.out.println("What is your first name?");
+		first = inputDevice.nextLine();
+		System.out.println("And what about your last name?");
+		last = inputDevice.nextLine();
+		System.out.println("How old are you?");
+		age = inputDevice.nextInt();
+		System.out.println("As long as you are over the age of " + age + " then");
+		System.out.println("you are free to pass, " + first + " " + last + "!");
+	}
+}
