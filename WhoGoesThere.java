1a12ee8fc16ab91406057171383ca7f52ae149e5
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-09-05 18:13:15 -0500

    MinusTen

--- WhoGoesThere.java
+++ WhoGoesThere.java
@@ -13,7 +13,7 @@ public class WhoGoesThere {
 		last = inputDevice.nextLine();
 		System.out.println("How old are you?");
 		age = inputDevice.nextInt();
-		System.out.println("As long as you are over the age of " + age + " then");
+		System.out.println("As long as you are over the age of " + age - 10 + " then");
 		System.out.println("you are free to pass, " + first + " " + last + "!");
 	}
 }
